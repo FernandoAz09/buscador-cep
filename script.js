a851c59601d2d19e7d105919e3306caca65708cd
Fernando <fsazevedo09@gmail.com>
2022-06-28 23:22:00 -0300

    Iniciated js

---
+++ script.js
@@ -0,0 +1,8 @@
+let inputCep = document.getElementById('cep')
+
+
+const pesquisarCep = () => {
+    return console.log('tá indo')
+}
+
+let subButton = document.getElementById('submit').addEventListener('click', pesquisarCep)
